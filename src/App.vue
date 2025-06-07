8121b12ff5dedb13c8ff8657be1c899a9745825a
Elcand <tjandraraharja99@gmail.com>
2025-06-07 08:55:04 +0700

    learn computed properties

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <script setup>
+import { reactive, ref, computed } from 'vue'
 
+const author = reactive({
+  name: 'JK Rowline',
+  books: ['Harry Kopter 1', 'Harry Kopter 2', 'Harry Kopter 3'],
+})
+
+const count = ref(4)
+const isPublished = computed(() => {
+  console.log(author.books)
+  return author.books.length > 0 ? 'Published yes' : 'Published no'
+})
+
+const addBooks = () => {
+  author.books.push('Harry Kopter ' + count.value )
+  count.value++
+}
 </script>
 
 <template>
-
+  <div>{{ isPublished }}</div>
+  <button @click="addBooks">{{ count }}</button>
 </template>
 
 <style scoped></style>
