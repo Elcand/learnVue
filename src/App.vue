7e9989567f5fe1990f2e775b9721a3bacdeaa3a2
Elcand <tjandraraharja99@gmail.com>
2025-06-07 09:29:29 +0700

    learn conditional rendering

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@ const author = reactive({
   books: ['Harry Kopter 1', 'Harry Kopter 2', 'Harry Kopter 3'],
 })
 
+const showAuthor = ref(false)
+
 const count = ref(4)
 const isPublished = computed(() => {
   console.log(author.books)
@@ -13,7 +15,7 @@ const isPublished = computed(() => {
 })
 
 const addBooks = () => {
-  author.books.push('Harry Kopter ' + count.value )
+  author.books.push('Harry Kopter ' + count.value)
   count.value++
 }
 </script>
@@ -21,6 +23,12 @@ const addBooks = () => {
 <template>
   <div>{{ isPublished }}</div>
   <button @click="addBooks">{{ count }}</button>
+  <h3>
+    {{ author.name }}
+    <span v-if="author.books.length > 1">Have more than a Books</span>
+    <span v-if="author.books.length ===1">Have Books</span>
+    <span v-else>Don't Have Books</span>
+  </h3>
 </template>
 
 <style scoped></style>
